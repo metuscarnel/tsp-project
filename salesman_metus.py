2eb79f5c0a67410fd5e345c220f274f72f5341ed
metus01 <carnelmetus@gmail.com>
2026-03-09 18:07:21 +0100

    generation de villes française

--- salesman_metus.py
+++ salesman_metus.py
@@ -1,33 +1,35 @@
-class City  :
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+from math import radians, cos, sin, sqrt, atan2
+import json, haversine, random
+def haversine_distance(ville1, ville2):
+    lat1, lon1 = ville1['latitude'], ville1['longitude']
+    lat2, lon2 = ville2['latitude'], ville2['longitude']
+    return haversine.haversine((lat1, lon1), (lat2, lon2))
 
-    def distance_to(self, other):
-        return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2 + (self.z - other.z) ** 2) ** 0.5
+class Ville:
+    def __init__(self, nom, latitude, longitude):
+        self.nom = nom
+        self.latitude = latitude
+        self.longitude = longitude
 
+def generate_random_villes(n):
+    villes = []
+    donnees_json = json.load(open("villes_france.json", "r", encoding="utf-8"))
+    
+    selection_aleatoire = random.sample(donnees_json, n)
 
-def departure_city():
-    return 
+    for item in selection_aleatoire:
+        nouvelle_ville = Ville(
+            nom=item["nom"],
+            latitude=item["latitude"],
+            longitude=item["longitude"]
+        )
+        villes.append(nouvelle_ville)
 
-def generate_cities(n):
-    import random
-    return [City(random.uniform(0, 100), random.uniform(0, 100), random.uniform(0, 100)) for _ in range(n)]
-cities = generate_cities(n) 
-
-def traveling_salesman(cities):
-    import random
-    best_route = None
-    best_distance = float('inf')
-
-    for _ in range(1000):  # Nombre d'itérations
-        route = cities[:]
-        random.shuffle(route)
-        distance = sum(route[i].distance_to(route[i + 1]) for i in range(len(route) - 1)) + route[-1].distance_to(route[0])
-        
-        if distance < best_distance:
-            best_distance = distance
-            best_route = route
-
-    return best_route, best_distance
+    return villes
+    
+    
+communes_france = json.load(open("villes_france.json", "r", encoding="utf-8"))
+print(haversine_distance(communes_france[0], communes_france[1]))
+print("Villes sélectionnées pour le trajet :")
+for v in generate_random_villes(10):
+    print(f"- {v.nom} (Lat:{v.latitude:.2f}, Lon:{v.longitude:.2f})")
